415e80b158378431e4b878418491973007d9b73a
lukasheinrich <lukas.heinrich@gmail.com>
2018-09-26 08:52:28 +0200

    TF

--- pyhf/tensor/tensorflow_backend.py
+++ pyhf/tensor/tensorflow_backend.py
@@ -54,7 +54,10 @@ class tensorflow_backend(object):
         tensor_in_1 = tensor_in_1 if tensor_in_2.dtype is not tf.bool else tf.cast(tensor_in_2, tf.float32)
         return tf.einsum('i,j->ij', tensor_in_1, tensor_in_2)
 
-    def astensor(self, tensor_in, dtype=tf.float32):
+    def gather(self,tensor,indices):
+        return tf.gather(tensor,indices)
+
+    def astensor(self, tensor_in, dtype='float'):
         """
         Convert to a TensorFlow Tensor.
 
@@ -64,6 +67,9 @@ class tensorflow_backend(object):
         Returns:
             `tf.Tensor`: A symbolic handle to one of the outputs of a `tf.Operation`.
         """
+        dtypemap = {'float': tf.float32, 'int': tf.int32}
+        dtype = dtypemap[dtype]
+
         if isinstance(tensor_in, tf.Tensor):
             v = tensor_in
         else:
