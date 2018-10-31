bc4fc54b6f62363c2dcfa050c4af5a6acdec5303
Giordon Stark <kratsg@gmail.com>
2018-10-30 17:03:45 -0700

    update tests and lumi needs a name

--- tests/test_import.py
+++ tests/test_import.py
@@ -35,13 +35,16 @@ def test_import_prepHistFactory():
     assert 'background1' in samples['channel1']
     assert 'background2' in samples['channel1']
 
-    assert pdf.spec['channels'][0]['samples'][2]['modifiers'][0]['type'] == 'staterror'
-    assert pdf.spec['channels'][0]['samples'][2]['modifiers'][0]['data'] == [0, 10.0]
+    assert pdf.spec['channels'][0]['samples'][1]['modifiers'][0]['type'] == 'lumi'
+    assert pdf.spec['channels'][0]['samples'][2]['modifiers'][0]['type'] == 'lumi'
 
-    assert pdf.spec['channels'][0]['samples'][1]['modifiers'][0]['type'] == 'staterror'
+    assert pdf.spec['channels'][0]['samples'][2]['modifiers'][1]['type'] == 'staterror'
+    assert pdf.spec['channels'][0]['samples'][2]['modifiers'][1]['data'] == [0, 10.0]
+
+    assert pdf.spec['channels'][0]['samples'][1]['modifiers'][1]['type'] == 'staterror'
     assert all(
         np.isclose(
-            pdf.spec['channels'][0]['samples'][1]['modifiers'][0]['data'], [5.0, 0.0]
+            pdf.spec['channels'][0]['samples'][1]['modifiers'][1]['data'], [5.0, 0.0]
         )
     )
 
@@ -51,10 +54,10 @@ def test_import_prepHistFactory():
     ]
 
     assert pdf.config.auxdata_order == sorted(
-        ['syst1', 'staterror_channel1', 'syst2', 'syst3']
+        ['lumi', 'syst1', 'staterror_channel1', 'syst2', 'syst3']
     )
 
-    assert data == [122.0, 112.0, 1.0, 1.0, 0.0, 0.0, 0.0]
+    assert data == [122.0, 112.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0]
 
     pars = pdf.config.suggested_init()
     pars[pdf.config.par_slice('SigXsecOverSM')] = [2.0]
@@ -82,4 +85,5 @@ def test_import_histosys():
         for channel in pdf.spec['channels']
     }
 
-    assert channels['channel2']['samples'][0]['modifiers'][0]['type'] == 'histosys'
+    assert channels['channel2']['samples'][0]['modifiers'][0]['type'] == 'lumi'
+    assert channels['channel2']['samples'][0]['modifiers'][1]['type'] == 'histosys'
